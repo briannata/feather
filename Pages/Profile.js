f071e2c148caf6aaaad2dcf5ab793fd111a800b8
Elaine Dong <elaine13@email.unc.edu>
2024-02-24 17:32:19 -0500

    create landing page

--- Pages/Profile.js
+++ Pages/Profile.js
@@ -1,11 +1,47 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, View, Text, Button, TouchableOpacity, Image} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-function Profile() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Profile!</Text>
+function Profile({onLogout}) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Profile!</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={onLogout}>
+          <Text style={styles.buttonText}>Log Out</Text>
+        </TouchableOpacity>
       </View>
-    );
-  }
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'start',
+      alignItems: 'center',
+      backgroundColor: '#F7F7E8',
+  },
+  buttonContainer: {
+      flexDirection: 'col',
+      justifyContent: 'space-between',
+      gap: '10px',
+      alignItems: 'center',
+      width: '80%',
+      marginTop: 10,
+      marginBottom: 10,
+  },
+  button: {
+      backgroundColor: '#45A9CD',
+      paddingVertical: 15,
+      borderRadius: 3,
+      width: '100%',
+  },
+  buttonText: {
+      color: '#fff',
+      fontSize: 16,
+      fontWeight: 'bold',
+      textAlign: 'center',
+  },
+});
 
 export default Profile;
