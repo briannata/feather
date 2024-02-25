faa6f6b10782989b1329554e4d721fcb1402cb0b
Trinh Kieu <tkieu@unc.edu>
2024-02-25 02:02:37 -0500

    added descriptions and birdDesc.json

--- Pages/Encyclopedia.js
+++ Pages/Encyclopedia.js
@@ -1,28 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TextInput, ActivityIndicator, ScrollView, Picker } from 'react-native';
+// import CollapsibleView from './CollapsibleView';
+import { AntDesign } from '@expo/vector-icons';
+import { Button } from 'react-native-web';
+// import Button from "@mui/material/Button";
+// import IconButton from '@mui/material';
+// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 
 function Encyclopedia() {
   const [isLoading, setLoading] = useState(true);
   const [birds, setBirds] = useState([]);
+  const [birdsDesc, setBirdsDesc] = useState([]);
+
   const [filteredBirds, setFilteredBirds] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedFilter, setSelectedFilter] = useState('All');
   const [familyCommonNames, setFamilyCommonNames] = useState([]);
+  const [expanded, setExpanded] = useState(false);
+
 
   const getBirds = async () => {
     try {
+      // fetching JSON from eBird API
       const response = await fetch('https://api.ebird.org/v2/ref/taxonomy/ebird?fmt=json&cat=species');
       const json = await response.json();
       console.log(json)
+      // Mapping the JSON data to variables to be used
       const birdData = json.map(bird => ({
         name: bird.comName,
         scientificName: bird.sciName,
         familyComName: bird.familyComName,
         order: bird.order
       }));
+
+      // slicing the BirdData to make data more manageable 
       setBirds(birdData.slice(0, 30));
-      setFilteredBirds(birdData.slice(0, 30));      
+      setFilteredBirds(birdData.slice(0, 30));  
+      
+      // connecting local birdDesc JSON and matching name up to bird names
+      const birdDesc = require('./birdDesc.json');
+      setBirdsDesc(birdDesc.birds.reduce((acc, bird) => {
+        // reduce - transform array into object 
+        // acc - stores result of each iteration and passes to next iteration
+        acc[bird.name] = bird.description;
+        return acc;
+      }, {}));
+
+      console.log(birdDesc.birds)
+
       const uniqueFamilyComNames = [...new Set(birdData.map(bird => bird.familyComName))];
       const sortedFamilyCommonNames = uniqueFamilyComNames.sort((a, b) => a.localeCompare(b));
       setFamilyCommonNames(sortedFamilyCommonNames);
@@ -87,6 +113,15 @@ function Encyclopedia() {
           <View key={bird.name} style={styles.birdContainer}>
             <Text style={styles.name}>{bird.name}</Text>
             <Text style={styles.scientificName}>{bird.scientificName}</Text>
+            <span className="showMore" onClick={() => setExpanded(!expanded)}>
+              <AntDesign name="down" size={24} color="black" />
+            </span>
+            {expanded ? (
+              <Text style={styles.scientificName}>
+                {/* description of bird */}
+                {birdsDesc[bird.name] || "No description available"}
+              </Text>
+            ) : null}
           </View>
         ))}
      </ScrollView>
@@ -97,13 +132,14 @@ function Encyclopedia() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'left',
+    alignItems: 'left',
   },
   searchInput: {
     width: '80%',
     padding: 10,
     marginBottom: 10,
+    marginLeft: 20,
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 5,
@@ -111,11 +147,13 @@ const styles = StyleSheet.create({
   filterPicker: {
     width: '80%',
     marginBottom: 10,
+    marginLeft: 20,
   },
   scrollView: {
     flexGrow: 1,
     justifyContent: 'left',
     alignItems: 'left',
+    marginLeft: 20,
   },
   birdContainer: {
     marginBottom: 20,
@@ -130,6 +168,15 @@ const styles = StyleSheet.create({
   scientificName: {
     fontSize: 16,
   },
+  // showMore: {
+  //   border: '1px solid black',
+  //   padding: '2px 5px',
+  //   cursor: 'pointer',
+  // },
+  // expandable: {
+  //   width: '40%',
+  //   margin: 'auto',
+  // },
 });
 
 export default Encyclopedia;
